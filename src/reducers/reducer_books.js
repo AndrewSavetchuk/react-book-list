3b5c8437da87121fee7bfce3d8a25b66e0a63836
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2018-01-19 17:51:39 +0200

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JavaScript: The good parts', pages: 900 },
+        { title: 'Harry Potter', pages: 490 },
+        { title: 'The Dark Tower', pages: 101 },
+        { title: 'Eloquent Ruby', pages: 10 }
+    ]
+}
